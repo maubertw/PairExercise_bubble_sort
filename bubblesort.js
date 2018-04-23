37d1b5bc1f0696b5ddad3975b9a90378bf28ab28
Heather Allyse <heather.haskin@outlook.com>
2018-04-23 11:22:57 -0500

    heather drove

---
+++ bubblesort.js
@@ -0,0 +1,33 @@
+//bubbleSort
+
+//[1,5,4,3,2]
+
+//takes an array and then
+
+
+//it calls compare
+//while iteration counter --- loop names a,b a = arr[i], arr[i+1]
+//counter--
+//outer looper for how many times to loop always starts at 1st elem: //has an iteration counter;
+//inner looper for looping through each element, minus 1 each iteration;
+
+
+//loop through every element, and compare higher or lower
+//if any element was swapped in that iteration, then iterate again
+//once a whole iteration happens with no swapping then return;
+//iteration counter starts at length of the array
+//on each iteration counter - 1 so you don't iterate over unnecessary ..
+
+//compare func(a,b) return arr[0] > arr[1] //this gives true of false;
+
+
+//swap function(a,b)
+// var elem;
+
+//if compare is true then swap;
+
+// elem =arr[0];
+// arr[0] = arr[1];
+// arr[1] = elem;
+
+
---
+++ bubblesort.spec.js
@@ -0,0 +1,21 @@
+describe('Swap', function(){
+  it('handles an empty array')
+})
+
+
+describe('Bubble Sort', function(){
+  it('handles an empty array', function(){
+    expect( bubbleSort([]) ).toEqual( [] );
+  });
+});
+
+
+//before each
+
+//it has a swap function
+//it has a bubble sort function
+//test how many swaps occur
+//test if the bubble sort given an array is equal to the expected sorted array
+
+
+
